5e6e8739d8cacd6f315e239474ff6382ffeb800f
Hugo <>
2020-10-23 18:02:05 +0800

    :construction: Get networkInformation for using it in conditional

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/af3ee5d0f9e1bbedc3fc/maintainability)](https://codeclimate.com/github/winston0410/react-slow-network-component/maintainability) [![Test Coverage](https://api.codeclimate.com/v1/badges/af3ee5d0f9e1bbedc3fc/test_coverage)](https://codeclimate.com/github/winston0410/react-slow-network-component/test_coverage) [![Known Vulnerabilities](https://snyk.io/test/github/winston0410/react-slow-network-component/badge.svg?targetFile=package.json)](https://snyk.io/test/github/winston0410/react-slow-network-component?targetFile=package.json) [![Codacy Badge](https://app.codacy.com/project/badge/Grade/40700abef1304f9fbd5bacf9e3c44570)](https://www.codacy.com/gh/winston0410/react-slow-network-component/dashboard?utm_source=github.com&utm_medium=referral&utm_content=winston0410/react-slow-network-component&utm_campaign=Badge_Grade)
 
-A component that only show content when the network type or the download speed is too slow.
+A component that selectively shows content when the user's network is too slow, utilizing [NetworkInformation API](https://developer.mozilla.org/en-US/docs/Web/API/NetworkInformation/downlink).
+
+## Installation
+
+```
+npm i @blackblock/react-slow-network-component
+```
+
+## Slow network definition
+
+TODO
--- src/index.js
+++ src/index.js
@@ -1,15 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {
+  returnIf
+} from '@blackblock/common-utilities'
 
-function SlowNetwork (props) {
-  console.log(NetworkInformation.downlink)
-  console.log(NetworkInformation.downlinkMax)
-  console.log(NetworkInformation.effectiveType)
-  console.log(NetworkInformation.saveData)
+const defaultDef = {
+  effectiveType: '2g',
+  downlink: '1',
+  saveData: true
+}
+
+function SlowNetwork ({ slowNetworkDef = defaultDef, returnElement }) {
+  const [slowConnectionFlag, setslowConnectionFlag] = useState(false)
+
+  useEffect(() => {
+    const networkInformation = navigator.connection || navigator.mozConnection || navigator.webkitConnection
+
+    const isSlowNetwork = ''
+
+    if (networkInformation.effectiveType) {
+
+    }
+
+    if (networkInformation.saveData === true) {
+      setslowConnectionFlag(true)
+    }
+  })
+
+  // const returnIf = function (predicate) {
+  //   return function (value1) {
+  //     return function (value2) {
+  //       return function (testValue) {
+  //         const $1 = predicate(testValue) === true
+  //
+  //         if ($1) {
+  //           return value1
+  //         }
+  //         return value2
+  //       }
+  //     }
+  //   }
+  // }
+
+  console.log(returnIf)
+
+  //
+  // const content = returnIf(
+  //   (x) => x === true
+  // )(<p>slow network</p>)(<p>fast network</p>)(slowConnectionFlag)
+
+  const content = ''
 
   return (
-    <p>
-      slow network
-    </p>
+    <>
+      { content }
+    </>
   )
 }
 
