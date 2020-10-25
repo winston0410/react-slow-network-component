788fa7f1b9f30f4e9518549c61dabddae6ebb383
kghugo <howtostudycantonese@gmail.com>
2020-10-26 00:05:35 +0800

    :construction: Use isSlowNetwork() from '@blackblock/slow-network-checker' to check if the connection is slow

--- src/index.js
+++ src/index.js
@@ -2,53 +2,29 @@ import React, { useEffect, useState } from 'react'
 import {
   returnIf
 } from '@blackblock/common-utilities'
+import {
+  isSlowNetwork
+} from '@blackblock/slow-network-checker'
 
 const defaultDef = {
   effectiveType: '2g',
-  downlink: '1',
-  saveData: true
+  downlink: 1,
+  saveData: true,
+  rtt: 10
 }
 
-function SlowNetwork ({ slowNetworkDef = defaultDef, returnElement }) {
+function SlowNetwork ({ slowNetworkDefinition = defaultDef, returnElement }) {
   const [slowConnectionFlag, setslowConnectionFlag] = useState(false)
 
   useEffect(() => {
     const networkInformation = navigator.connection || navigator.mozConnection || navigator.webkitConnection
+    const isConnectionSlow = isSlowNetwork(slowNetworkDefinition)(networkInformation)
+    setslowConnectionFlag(isConnectionSlow)
+  }, [])
 
-    const isSlowNetwork = ''
-
-    if (networkInformation.effectiveType) {
-
-    }
-
-    if (networkInformation.saveData === true) {
-      setslowConnectionFlag(true)
-    }
-  })
-
-  // const returnIf = function (predicate) {
-  //   return function (value1) {
-  //     return function (value2) {
-  //       return function (testValue) {
-  //         const $1 = predicate(testValue) === true
-  //
-  //         if ($1) {
-  //           return value1
-  //         }
-  //         return value2
-  //       }
-  //     }
-  //   }
-  // }
-
-  console.log(returnIf)
-
-  //
-  // const content = returnIf(
-  //   (x) => x === true
-  // )(<p>slow network</p>)(<p>fast network</p>)(slowConnectionFlag)
-
-  const content = ''
+  const content = returnIf(
+    (x) => x === true
+  )(<p>slow network</p>)(<p>fast network</p>)(slowConnectionFlag)
 
   return (
     <>
